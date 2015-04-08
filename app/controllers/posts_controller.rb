baf7d23f06b0a0aa9499e1b48e9a1c67436a87a5
DumboCL <wyz513@gmail.com>
2015-04-08 11:19:28 +0800

    finished the CRUD of Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+  
   def index
     @posts = Post.all
   end
@@ -9,19 +11,41 @@ class PostsController < ApplicationController
   end
 
   def new
-    
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
 
+    if @post.save
+      flash[:notice] = "Your post was created."
+      redirect_to posts_path
+    else
+      flash[:notice] = @post.errors
+      render 'new'
+    end
   end
 
   def edit
-
+    @post = Post.find(params[:id])
   end
 
   def update
-
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "The post was updated"
+      redirect_to posts_path
+    else
+      render :edit
+    end
   end
 
+  private
+  def set_post
+    @post = Post.find(params[:id])
+    
+  end
+  def post_params
+    params.require(:post).permit!
+  end
 end
