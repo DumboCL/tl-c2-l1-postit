5b3e788d81916da405216d7eb724b6eae7b85f0c
DumboCL <wyz513@gmail.com>
2015-04-16 16:43:47 +0800

    helper with display_datetime format

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,6 @@ class PostsController < ApplicationController
   end
 
   def create
-    
     @post = Post.new(post_params)
     @post.creator = User.first #TODO
 
@@ -28,7 +27,6 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @categories = Category.all
   end
 
   def update
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,9 @@
 module ApplicationHelper
   def fix_url(str)
     str.starts_with?('http://') ? str : "http://#{str}"
-    
+  end
+
+  def display_datetime(time)
+    time.strftime("西元%Y年%m月%d日%H:%M:%S") #西元2015年04月04日14:27:08
   end
 end
